e9fc4dce1f5084124ac00dcffa2f0a99c8aa8a1d
Pranav Kishan <51042522+PranavKishan213@users.noreply.github.com>
2021-01-20 18:27:04 +0530

    Add project files.

---
+++ launcher/AppMain.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+/// <summary>
+/// Interaction logic for Window1.xaml
+/// </summary>
+/// 
+using System.Windows.Navigation;
+
+namespace launcher
+{
+    public partial class AppMain : Window
+    {
+        string mainUser, mainPass, mainPath, mainRegion;
+        public AppMain()
+        {
+            InitializeComponent();
+        }
+        public void DataReceive(string user, string pass, string path, string region)
+        {
+            mainUser = user;
+            mainPass = pass;
+            mainPath = path;
+            mainRegion = region;
+            Console.WriteLine("The username, password and path entered is: " + mainUser + ", " + mainPass + ", " + mainPath + " and " + mainRegion);
+        }
+
+        public void StartGame(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = mainPath;
+            proc.Start();
+
+
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void News_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://runitback.gg/");
+
+        }
+
+        private void Leaderboard(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://blitz.gg/valorant/leaderboards/general");
+        }
+
+        private void guides(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://blitz.gg/valorant/guides");
+        }
+
+        private void valtrack(object sender, RoutedEventArgs e)
+        {
+            mainViewsys.Source = new Uri("/tracker.xaml", UriKind.Relative);
+        }
+
+        private void abt(object sender, RoutedEventArgs e)
+        {
+            mainViewsys.Source = new Uri("/about.xaml", UriKind.Relative);
+        }
+    }
+}
---
+++ launcher/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.IO;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool detect = false;
+        string filem = "D:\\txtjson.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists(filem)){
+                StreamReader file = File.OpenText(filem);
+                JsonSerializer jsSer = new JsonSerializer();
+                datanum dm = (datanum)jsSer.Deserialize(file, typeof(datanum));
+                username.Text = dm.usernamecs;
+                password.Password = dm.passwordcs;
+                path.Text = dm.pathcs;
+                region.Text = dm.regioncs;
+                detect = true;
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            if (detect == true)
+            {
+                datanum dn = new datanum();
+                AppMain ap = new AppMain();
+                dn.usernamecs = username.Text;
+                dn.passwordcs = password.Password.ToString();
+                dn.pathcs = path.Text;
+                dn.regioncs = region.Text;
+                this.Close();
+                ap.DataReceive(dn.usernamecs, dn.passwordcs, dn.pathcs, dn.regioncs);
+            }
+            else
+            {
+                datanum dn = new datanum();
+                dn.usernamecs = username.Text;
+                dn.passwordcs = password.Password.ToString();
+                dn.pathcs = path.Text + "\\VALORANT.lnk";
+                dn.regioncs = region.Text;
+                AppMain ap = new AppMain();
+                this.Close();
+                ap.DataReceive(dn.usernamecs, dn.passwordcs, dn.pathcs, dn.regioncs);
+                string output = JsonConvert.SerializeObject(dn, Formatting.Indented);
+                Console.WriteLine(output);
+                System.IO.File.WriteAllText(filem, output);
+            }
+
+
+        }
+    }
+
+    public class datanum
+    {
+        public string usernamecs { get; set; }
+        public string passwordcs { get; set; }
+        public string pathcs { get; set; }
+        public string regioncs { get; set; }
+    }
+}
